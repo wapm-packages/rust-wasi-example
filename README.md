0f0af99bc9967ceaac7e98b2090ce48d140ad1a9
Mark McCaskey <mark@wasmer.io>
2019-04-04 15:20:19 -0700

    Add example program, readme; update gitignore

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# rust-wasi-example
-A toy application compiled to WASI Web Assembly
+# Rust WASI Example
+
+A toy program implementing [HQ9+][3] to demo compiling Rust to [WASI][1] and running it with [Wasmer][2].
+
+## Setting up
+
+Ensure you have an up to date version of Rust nightly and run:
+
+```
+rustup target add wasm32-unknown-wasi --toolchain nightly
+```
+
+## Building
+
+```
+cargo +nightly build --target=wasm32-unknown-wasi
+cp target/wasm32-unknown-wasi/debug/wasi-example.wasm .
+```
+
+## Running
+
+```
+cd ../wasmer # go to Wasmer source directory
+cargo run --release --features="wasi" -- run ../rust-wasi-example/wasi-example.wasm -- -e "HQ9+"
+```
+
+[1]: https://hacks.mozilla.org/2019/03/standardizing-wasi-a-webassembly-system-interface/
+[2]: https://github.com/wasmerio/wasmer
+[3]: https://esolangs.org/wiki/HQ9%2B
---
+++ src/main.rs
@@ -0,0 +1,60 @@
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    /// Evaluate HQ9+ source code
+    #[structopt(short = "e", long = "eval")]
+    source_code: Option<String>,
+    /// Force evaluation of the `H` instruction
+    #[structopt(short = "h", long = "hello-world")]
+    hello_world: bool,
+    /// Force evaluation of the `9` instruction
+    #[structopt(short = "9", long = "nintey-nine-bottles")]
+    ninety_nine_bottles: bool,
+}
+
+fn print_hello_world() {
+    println!("Hello, world!\n");
+}
+
+fn print_nintey_nine_bottles_of_beer_on_the_wall() {
+    for i in (1..100).rev() {
+        println!("{} bottles of beer on the wall,\n{} bottles of beer.\nTake one down, pass it around,\n{} bottles of beer on the wall.", i, i, i - 1);
+    }
+}
+
+fn run(source_code: String) {
+    #[allow(unused_variables)]
+    let mut accumulator = 0;
+
+    for c in source_code.chars() {
+        match c {
+            'H' => print_hello_world(),
+            '9' => print_nintey_nine_bottles_of_beer_on_the_wall(),
+            'Q' => println!("{}\n", source_code),
+            '+' => accumulator += 1,
+            _ => (),
+        }
+    }
+}
+
+fn main() {
+    let opt = Opt::from_args();
+
+    if opt.hello_world {
+        print_hello_world();
+        return;
+    }
+
+    if opt.ninety_nine_bottles {
+        print_nintey_nine_bottles_of_beer_on_the_wall();
+        return;
+    }
+
+    if let Some(src) = opt.source_code {
+        run(src);
+    } else {
+        eprintln!("Error: please pass HQ9+ source code via the `-e` flag");
+        ::std::process::exit(-1);
+    }
+}
